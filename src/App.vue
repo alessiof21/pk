1ce2c7a83a127640ae462558bbb1219210d7309d
alessiof21 <alexeyfedorenko21@mail.ru>
2023-11-13 18:13:00 +0300

    fix: minor typos in code

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,401 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="main__flex-container">
+    <div class="flex__list">
+        <ul v-for="list in lists" :id="list.id" @click="chooseList(list.id)"> 
+          <div class="checkbox-flex">
+            <div :id="list.id" class="my-checkbox" :class="list.checked ? 'dot' : 'none'" @click="checkList(list.id)"></div>
+            <p class="title__text"> list {{ list.id }}</p>
+          </div>
+          <li v-for="elem in list.elements" :hidden="chosenList !== list.id">
+            <div class="item__flex-container">
+              <span class="item__elements">
+                <input :id="`list${list.id}_elem${elem.id}`" type="checkbox" :checked="elem.checked" @click="elem.checked = !elem.checked"/> 
+                <label :for="`list${list.id}_elem${elem.id}`">item {{ elem.id }}</label>
+              </span>
+              <span class="item__elements">
+                <span class="options__flex-container">
+                  <input class="input__number" :id="`number_list${list.id}_elem${elem.id}`" min="0" max="99" type="number" @change="elem.number=+$event.target.value" :value="elem.number"/>
+                  <input class="item__square" :id="`color_list${list.id}_elem${elem.id}`" type="color" :style="`border: 0px solid ${elem.color};`" :value="elem.color" @change="elem.color=$event.target.value"/>
+                </span>
+              </span>
+            </div>
+          </li>
+        </ul>
+    </div>
+    <div class="flex__list">
+      <div v-for="list in lists" class="list__elements">
+        <div class="list__flex-container">
+          <div class="list__title">
+            <div class="title__flex">
+              <p class="title__text">list {{ list.id }}</p>
+              <button v-if="chosenList === list.id" class="title__button" @click="sorted = !sorted"> {{ sorted !== true ? 'Сортировать' :  'Перемешать'}}</button>
+            </div>
+          </div>
+          <div class="list__body" v-if="chosenList === list.id">
+            <div v-if="sorted">
+               <div class="body__flex">
+                  <div v-for="elem in list.elements" :key="elem.id" class="color-line">
+                    <div v-if="elem.checked" class="color__flex-container">
+                      <div class="item__square" v-for="i in elem.number" :key="i" :style="`background-color: ${elem.color}`" @click="--elem.number">
+                      </div>
+                    </div>
+                  </div>
+               </div> 
+            </div>
+            <div v-else>
+              <div class="color-line">
+                <div class="color__flex-container">
+                  <div class="item__square" v-for="elem in unsorted"  :style="`background-color: ${elem}`" @click="deleteItem(elem)"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </template>
 
+<script>
+  export default {
+    data() {
+      return {
+        chosenList: 0, // Переменная, фиксирующая, какой из list мы спортим
+        sorted: false, // Переменная, фиксирующая, отсортировано ли состояние
+        lists: [
+          {
+            id: 1,
+            checked: false,
+            elements: [
+              {
+                id: 1,
+                number: 20,
+                checked: false,
+                color: '#FF0000'
+              },
+              {
+                id: 2,
+                number: 2,
+                checked: false,
+                color: '#008000'
+              },
+              {
+                id: 3,
+                number: 6,
+                checked: false,
+                color: '#FFFF00'
+              },
+              {
+                id: 4,
+                number: 9,
+                checked: false,
+                color: '#0000FF'
+              }
+            ]
+          },
+          {
+            id: 2, 
+            checked: false,
+            elements: [
+              {
+                id: 1,
+                number: 5,
+                checked: false,
+                color: '#FF0000'
+              },
+              {
+                id: 2,
+                number: 8,
+                checked: false,
+                color: '#008000'
+              },
+              {
+                id: 3,
+                number: 3,
+                checked: false,
+                color: '#FFFF00'
+              },
+              {
+                id: 4,
+                number: 6,
+                checked: false,
+                color: '#0000FF'
+              }
+            ]
+          },
+          {
+            id: 3,             
+            checked: false,
+            elements: [
+              {
+                id: 1,
+                number: 7,
+                checked: false,
+                color: '#FF0000'
+              },
+              {
+                id: 2,
+                number: 4,
+                checked: false,
+                color: '#008000'
+              },
+              {
+                id: 3,
+                number: 2,
+                checked: false,
+                color: '#FFFF00'
+              },
+              {
+                id: 4,
+                number: 1,
+                checked: false,
+                color: '#0000FF'
+              }
+            ]
+          },
+          {
+            id: 4,            
+            checked: false,
+            elements: [
+              {
+                id: 1,
+                number: 6,
+                checked: false,
+                color: '#FF0000'
+              },
+              {
+                id: 2,
+                number: 6,
+                checked: false,
+                color: '#008000'
+              },
+              {
+                id: 3,
+                number: 3,
+                checked: false,
+                color: '#FFFF00'
+              },
+              {
+                id: 4,
+                number: 9,
+                checked: false,
+                color: '#0000FF'
+              }
+            ]
+          },
+          {
+            id: 5, 
+            checked: false,
+            elements: [
+              {
+                id: 1,
+                number: 2,
+                checked: false,
+                color: '#FF0000'
+              },
+              {
+                id: 2,
+                number: 7,
+                checked: false,
+                color: '#008000'
+              },
+              {
+                id: 3,
+                number: 10,
+                checked: false,
+                color: '#FFFF00'
+              },
+              {
+                id: 4,
+                number: 5,
+                checked: false,
+                color: '#0000FF'
+              }
+            ]
+          }
+        ]
+      }
+    },
+    methods: {
+      chooseList(id) {
+        if (this.chosenList !== id) { // Если кликаем по другой вкладке списка list, которая еще не выбрана
+          this.chosenList = id; 
+          this.sorted = false;
+        }
+      },
+      checkList(id) {
+        if (id !== this.chosenList) { // Если кликаем по чекбоксу, который не открыт, то не делаем выбор/снятие выбора с элементов
+          return;
+        }
+        let flag = 0;
+        const maxFlag = this.lists[id-1].elements.length;
+        for (let i = 0; i < this.lists[id-1].elements.length; i++) {
+          if (this.lists[id-1].elements[i].checked === true) {
+            ++flag;
+          }
+        }
+        if (this.lists[id-1].checked === true) {
+          this.lists[id-1].checked = false;
+        } else {
+          if (flag === 0) { // Ничего из item не выделено
+              for (let i = 0; i < this.lists[id-1].elements.length; i++) {
+                this.lists[id-1].elements[i].checked = true;
+              }
+          } else if (flag === maxFlag) { // Выделены все item
+              for (let i = 0; i < this.lists[id-1].elements.length; i++) {
+                this.lists[id-1].elements[i].checked = false;
+              }
+          } else {
+            if (this.lists[id-1].checked === false) {
+              this.lists[id-1].checked = true;
+            } 
+          }
+        }
+      },
+      deleteItem(color) {
+        for (let i = 0; i < this.lists[this.chosenList-1].elements.length; i++) {
+          if (this.lists[this.chosenList-1].elements[i].color === color) {
+            return --this.lists[this.chosenList-1].elements[i].number;
+          }
+        }
+      }
+    },
+    computed: {
+      unsorted() {
+        if (this.chosenList !== 0) {
+          const colors = [];
+          for (let i = 0; i < this.lists[this.chosenList-1].elements.length; i++) {
+            if (this.lists[this.chosenList-1].elements[i].checked === true) {
+              const arr = Array(this.lists[this.chosenList-1].elements[i].number).fill(this.lists[this.chosenList-1].elements[i].color);
+              colors.push(...arr);
+            }
+          }
+          colors.sort(() => Math.random() - 0.5);
+          return colors;
+        }
+      }
+
+    }   
+  }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  .main__flex-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+  }
+  .flex__list {
+    border: 2px solid black;
+    width: 30%;
+  }
+  .item__flex-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1vh 4vw 1vh 2vw;
+  }
+  .checkbox-flex {
+    display: flex;
+    flex-direction: row;
+    justify-content: start;
+    align-items: center;
+  }
+  .title__text {
+    margin-left: 5px;
+  }
+  .title__button {
+    border-radius: 5px;
+    border: 2px solid rgb(34, 140, 240);
+    background: transparent;
+    color: #fff;
+    background: rgb(34, 140, 240);
+  }
+  .title__button:hover {
+    opacity: 0.7;
+  }
+  .title__button:active{
+    opacity: 1;
+  }
+  .my-checkbox {
+    width: 20px;
+    height: 20px;
+    margin: 2px;
+  }
+  .none {
+    background-image: url('@/img/nonchecked.png');
+    background-size: cover;
+  }
+  .dot {
+    background-image: url('@/img/checked-dot.png');
+    background-size: cover;
+  }
 
-nav {
-  padding: 30px;
-}
+  .item__elements {
+    width: 30%;
+  }
+  .options__flex-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .input__number {
+    width: 50%;
+  }
+  .item__square {
+    width: 15px;
+    height: 15px;
+    margin: 2px 2px;
+  }
+  ul {
+    list-style-type: none;
+  }
+  input[type="color"] {
+    padding: 0;
+  }
+  input[type="color" i]::-webkit-color-swatch-wrapper {
+    padding: 0;
+  }
+  .list__elements {
+    margin: 2vh 2vw;
+    border: 1px solid black;
+    height: auto;
+  }
+  .list__flex-container {
+    display: flex;
+    flex-direction: column;
+  }
+  .list__title {
+    margin: 2px;
+  }
+  .list__body {
+    margin: 2px;
+  }
+  .title__flex {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .body__flex {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+  }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  .color-line {
+    width: 100%;
+    height: 100%;
+    margin-bottom: 5px;
+  }
+  .color__flex-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: start;
+    flex-wrap: wrap;
+  }
 </style>
